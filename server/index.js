917c41cf3ca7cf66aacf477cb7a031abd65d75ff
zeevana <sromdoni0@gmail.com>
2026-03-03 05:16:24 +0900

    api

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 function adminAuth(req, res, next) {
   const key = req.headers["x-admin-key"];
 
-  if (!key || key !== process.env.ADMIN_KEY) {
+  if (!key || key !== process.env.VITE_ADMIN_KEY) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
