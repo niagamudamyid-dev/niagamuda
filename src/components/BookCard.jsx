8069049e3389ad5fe5df7c6a96432913f8c62ed6
zeevana <sromdoni0@gmail.com>
2026-03-14 14:09:10 +0900

    api

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -4,10 +4,14 @@ return (
 
 <div className="card">
 
+<div className="card-cover">
+
 <img
 src={book.image || "https://via.placeholder.com/300x400"}
 alt={book.title}
 />
+
+</div>
 
 <div className="card-sub">
 {book.subcategory || book.category}
